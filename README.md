2362470fa65903e2f5da6a3bb01d19d4d988bfa5
Jonathan T L Lee <jono-lee@hotmail.co.uk>
2018-09-26 13:48:37 +0100

    project

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-budgie-launcher-super
+# budgie-launcher-super
+## idea/why
+Basically i needed a command to switch tasks, given i'm used to the same functionality with windows. me tones more productive than tabbing.
+
+Its simple switch to or launch window from tasklist by using super shortcuts
+```
+Super + 1 : windows 1
+Super + 2 : windows 2
+Super + 3 : windows 3
+...9
+```
+I've looked at the source code for budgie-desktop, and to implement in valac, c is naturally hard me not knowing anything about c. And not being able to get a build of budgie-desktop running with my machine. So I've used nodejs
+
+## Install
+```
+npm install -g budgie-launcher-switcher
+```
+## API
+```bash
+# Super + 1
+budgie-launcher-switcher 1
+# Super + 2
+budgie-launcher-switcher 2
+# Super + 3
+budgie-launcher-switcher 3
+```
+
+### --launch
+Launch the task again despite window being open, handy to open an a new instance of `window 1` i've linked to `Shift + Super + 1`
+```bash
+budgie-launcher-switcher --launch 1
+```
---
+++ index.js
@@ -0,0 +1,200 @@
+const fs = require('fs-extra')
+const number2str = require('number2str')
+const sh = require('shell-exec')
+const ps = require('ps')
+const awaity = require('awaity')
+
+const appPath = '/usr/share/applications/'
+const ignoreCommand = new Set(['budgie-panel', 'budgie-helper'])
+
+const formatCommand = (s) => {
+  const sEndBit = s.split('/').pop()
+  if (sEndBit === 'gnome-terminal-') {
+    return 'gnome-terminal'
+  }
+  return sEndBit
+}
+
+const getPinned = async () => {
+  const path = '/com/solus-project/budgie-panel/instance/icon-tasklist/'
+  const sh1 = await sh(`dconf list ${path}`)
+  const panelId = sh1.stdout.trim()
+  const sh2 = await sh(`dconf read ${path}${panelId}pinned-launchers`)
+
+  // eslint-disable-next-line
+  const aPanel = Function(`"use strict";return (${sh2.stdout.trim()})`)()
+  return aPanel
+}
+
+const launch = async (desktopName) => {
+  const desktopExits = await fs.pathExists(`${appPath}${desktopName}`)
+  if (!desktopExits) {
+    throw Error('Could not find path: ' + desktopName)
+  }
+  try {
+    await sh(`gtk-launch ${desktopName}`)
+  } catch (err) {
+
+  }
+
+  // const tmpName = `launcher-${number2str.random(6)}.desktop`
+  // const filePath = `${process.env.HOME}/.local/share/applications/${tmpName}`
+  // const pathExists = await fs.pathExists(filePath)
+  // if (pathExists) {
+  //   return launch(desktopName)
+  // }
+
+  // await fs.copy(desktopPath, filePath)
+  // const rmTmp = () => {
+  //   fs.unlink(filePath)
+  // }
+  // sh(`gtk-launch ${tmpName}`).then(rmTmp, rmTmp)
+}
+
+const getApplictaionsExecPath = async () => {
+  const files = await fs.readdir(appPath)
+  const apps = files.filter(s => s.match(/\.desktop$/) !== null)
+  const o = {
+    DesktopExe: {},
+    ExeDesktop: {},
+    CommandDesktop: {},
+    DesktopCommand: {}
+  }
+  await awaity.mapLimit(
+    apps,
+    async sDesktop => {
+      const file = await fs.readFile(`${appPath}${sDesktop}`)
+      let sExec = file
+        .toString()
+        .split('\n')
+        .find(s => /^Exec=/.test(s))
+      const aExec = sExec.split(/Exec=|\s/)
+      aExec.shift()
+      sExec = aExec.join(' ')
+      const sCommand = formatCommand(aExec.shift())
+      // o.DesktopExe[sDesktop] = sExec
+      // o.ExeDesktop[sExec] = sDesktop
+      // o.CommandDesktop[sCommand] = sDesktop
+      o.DesktopCommand[sDesktop] = sCommand
+    },
+    12
+  )
+  return o
+}
+
+const getWindows = async () => {
+  const sh1 = await sh(`wmctrl -lp`)
+  const windows = sh1.stdout
+    .trim()
+    .split('\n')
+    .map(line => {
+      const items = line.split(/\s+/)
+      return {
+        hexWID: items[0],
+        wid: parseInt(items[0], 16),
+        desktop: items[1],
+        pid: Number(items[2]),
+        user: items[3],
+        title: items.slice(4).join(' ')
+      }
+    })
+  const procs = await ps({ pid: windows.map(o => o.pid) })
+  return windows
+    .map(o => {
+      const proc = procs.find((proc) => (Number(proc.pid) === o.pid))
+      if (!proc) {
+        return
+      }
+      o.command = formatCommand(proc.command)
+      return o
+    })
+    .filter(window => (window !== undefined))
+    .filter(window => (!ignoreCommand.has(window.command)))
+}
+
+const main = async ({
+  superNumber = 3,
+  windowIndex = 0,
+  bLaunch = false,
+  bIncrementWindowIndex = true,
+  attemptIndex = 0
+}) => {
+  if (attemptIndex > 2) {
+    return
+  }
+  const [
+    pinned,
+    windows,
+    execPaths,
+    sh2
+  ] = await awaity.map([
+    getPinned(),
+    getWindows(),
+    getApplictaionsExecPath(),
+    sh(`xdotool getactivewindow`)
+  ])
+  const activeWID = Number(sh2.stdout.trim())
+  const pinnedAsCommands = pinned.map((sDesktop) => {
+    return execPaths.DesktopCommand[sDesktop]
+  })
+  const pinnedIndex = pinnedAsCommands.reduce((o, sCommand, i, arr) => {
+    o[sCommand] = i
+    return o
+  }, {})
+
+  const aWindowsSorted = []
+  let activeIndex = []
+  windows.forEach((window, i) => {
+    window.i = i
+    window.active = window.wid === activeWID
+    let index = pinnedIndex[window.command]
+    if (index === undefined) {
+      index = pinnedAsCommands.length - 1
+      while (index < aWindowsSorted.length) {
+        index++
+        if (Array.isArray(aWindowsSorted[index]) && aWindowsSorted[index][0].command === window.command) {
+          break
+        }
+      }
+    }
+    if (!aWindowsSorted[index]) {
+      aWindowsSorted[index] = []
+    }
+    aWindowsSorted[index].push(window)
+    if (window.active) {
+      activeIndex = [index, aWindowsSorted[index].length - 1]
+    }
+  })
+  let superIndex = superNumber - 1
+  const notLaunched = aWindowsSorted[superIndex] === undefined
+  if (pinned[superIndex] !== undefined && (bLaunch || notLaunched)) {
+    await launch(pinned[superIndex])
+    windowIndex = notLaunched ? 0 : aWindowsSorted[superIndex].length
+    await main({
+      superNumber,
+      bLaunch: false,
+      bIncrementWindowIndex: false,
+      attemptIndex: attemptIndex + 1,
+      windowIndex
+    })
+    return
+  }
+
+  if (bIncrementWindowIndex && activeIndex[0] === superIndex) {
+    windowIndex = (activeIndex[1] + 1) % aWindowsSorted[superIndex].length
+  }
+  // switch focus to window
+  if (aWindowsSorted[superIndex] && aWindowsSorted[superIndex][windowIndex]) {
+    const window = aWindowsSorted[superIndex][windowIndex]
+    await sh(`wmctrl -ia ${window.hexWID}`)
+  }
+}
+
+let superNumber = Number(process.argv[process.argv.length - 1])
+let bLaunch = process.argv[process.argv.length - 1] === '--launch'
+
+// superNumber = 1
+// bLaunch = true
+if (Number.isSafeInteger(superNumber) && superNumber > 0) {
+  main({ superNumber, bLaunch })
+}
